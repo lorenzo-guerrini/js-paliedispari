e9d66e5f768dbcbef846f5674587bdc238a7ef35
lorenzo-guerrini <lorenzoguerrini8@gmail.com>
2021-12-19 16:19:09 +0100

    step 1 completato

--- pari-dispari/script.js
+++ pari-dispari/script.js
@@ -1,6 +1,21 @@
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. - FATTO
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+let inputEvenOdd = "";
+
+do {
+    inputEvenOdd = prompt("Pari o dispari?").toLowerCase();
+
+} while (!(inputEvenOdd == "pari" || inputEvenOdd == "dispari"))
+
+let inputNumber = 0;
+
+do {
+    inputNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+} while (inputNumber < 1 || inputNumber > 5 || isNaN(inputNumber));
+
+console.log(inputNumber);
