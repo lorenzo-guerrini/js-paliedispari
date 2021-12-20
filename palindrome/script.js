84ff41c400bcad777c669a59eb4df88e706795d9
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-20 10:05:05 +0100

    inserita rimozione spazi e correzioni varie

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,33 +1,39 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-const input = prompt("Inserisci una parola").toLowerCase();
+const input = prompt("Inserisci una parola o una frase");
 
 function isPalindrome(word) {
 
-    //Controlla che sia stata effettivamente inserita una parola
+    //Controlla che sia stata effettivamente inserito qualcosa e trasforma in lowercase
     if (word == "") {
         return false;
+    }
+    word = word.toLowerCase();
+
+    //Rimuove gli spazi dall'input inserito
+    let wordNoSpaces = "";
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] !== " ") {
+            wordNoSpaces += word[i];
+        }
     }
 
     //Inverte la parola inserita salvandola in una variabile
     let wordReversed = "";
-    let wordLenght = word.length;
-
-    for (let i = wordLenght - 1; i >= 0; i--) {
-        wordReversed += word[i];
+    for (let i = wordNoSpaces.length - 1; i >= 0; i--) {
+        wordReversed += wordNoSpaces[i];
     }
 
     //Controlla che la parola inserita sia uguale a quella invertita
-    if (word == wordReversed) {
+    if (wordNoSpaces == wordReversed) {
         return true;
     }
-
     return false;
 }
 
 if (isPalindrome(input)) {
-    console.log(input + " è una parola palindroma!")
+    console.log(input + " è palindroma!")
 } else {
-    console.log(input + " non è una parola palindroma!")
+    console.log(input + " non è palindroma!")
 }
